180a4a6de77cd0c781f6146485fc01ce83a34d9e
Piyush <piyusharora596@gmail.com>
2022-03-14 22:30:33 +0530

    Changed Design

--- scripts/app.js
+++ scripts/app.js
@@ -1,76 +1,42 @@
 // basic details of the event to be added
 class userCustomOccasions {
-    constructor(eventName, date, month) {
+    constructor(id, eventName, date) {
+        this.id = id;
         this.eventName = eventName;
         this.date = date;
-        this.month = month;
     }
 }
 
 
 //does UI events
 class UI {
-    static openForm() {
-        document.getElementById("myForm").style.display = "block";
-    }
-
-    static closeForm() {
-        if (document.getElementById("myForm").style.display === "block") {
-            document.getElementById("myForm").style.display = "none";
-            UI.clearFields();
-        }
-    }
-
-    static loadAppElements(date, month, nonFormContainers) {
-        for (let i = 1; i <= 31; i++) {             //displays dates in numbers
-            const add_date = document.createElement('option');
-            if (i < 10) {
-                add_date.textContent = '0' + i;
-            }
-            else add_date.textContent = i;
-            add_date.value = i;
-            date.append(add_date);
-        }
-
-        for (let i = 1; i <= 12; i++) {               //displays months in numbers
-            const add_month = document.createElement('option');
-            if (i < 10) {
-                add_month.textContent = '0' + i;
-            }
-            else add_month.textContent = i;
-            add_month.value = i;
-            month.append(add_month);
-        }
-
-        // this handles add event form closing on page if clicked anywhere else rather than form
-        for (let i = 0; i < nonFormContainers.length; i++) {
-            nonFormContainers[i].addEventListener('click', UI.closeForm);
-        }
-    }
-
     static loadUserEvents() {
         let events = Store.getUserEvents();
         events.forEach((event) => UI.addEvent(event));
     }
 
     static addEvent(eventDetails) {
-        const eventsList = document.querySelector('.events-list');
-        const addEventCard = document.createElement('div');     //creating the div block of card to display on the events-list container
-        addEventCard.classList.add('card', 'text-white', 'bg-dark', 'm-3');
-        addEventCard.style.maxWidth = '20rem';
-        addEventCard.innerHTML = `                            
-            <div class= "card-body">
-                <h4 class= "card-title p-10">${eventDetails.eventName}</h4>
-                <button type="button" class="btn btn-primary btn-sm delete-event">X</button>
-            </div>
+        const list = document.querySelector('#events-list');
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>
+            ${eventDetails.id}
+        </td>
+        <td>
+            ${eventDetails.eventName}
+        </td>
+        <td>
+            ${eventDetails.date}
+        </td>
+        <td>
+            <button type="button" class="btn btn-danger remove">X</button>
+        </td>
         `;
-        eventsList.appendChild(addEventCard);               //adding the block to events list 
+        list.appendChild(row);
     }
 
     static deleteEvent(eventTarget) {
-        if (eventTarget.classList.contains('delete-event')) {
-            eventTarget.parentElement.parentElement.remove();
-        }
+        eventTarget.remove();
     }
 
     static showAlert(message, status) {
@@ -80,16 +46,15 @@ class UI {
         div.appendChild(document.createTextNode(message));
         div.style.zIndex = -9;
         const container = document.querySelector('body');
-        const header = document.querySelector('.header');
+        const header = document.querySelector('.container');
         container.insertBefore(div, header);
 
-        setTimeout(() => document.querySelector('.alert').remove(), 800);          //vanishes in 0.8 sec
+        setTimeout(() => document.querySelector('.alert').remove(), 1000);          //vanishes in 1 sec
     }
 
-    static clearFields() {
-        document.querySelector('#event-name').value = '';
-        document.querySelector('#date').value = 'DATE';
-        document.querySelector('#month').value = 'MONTH';
+    static clearFields(eventName, eventDate) {
+        eventName.value = '';
+        eventDate.value = '';
     }
 
     static displayCount(event) {
@@ -140,10 +105,11 @@ class Store {
         localStorage.setItem('events', JSON.stringify(events));
     }
 
-    static deleteEvent(customEventName) {
+    static deleteEvent(id) {
         const events = Store.getUserEvents();
+        console.log(events);
         events.forEach((event, index) => {
-            if (event.eventName === customEventName) {
+            if (event.id == id) {
                 events.splice(index, 1);
             }
         });
@@ -151,40 +117,38 @@ class Store {
     }
 }
 
-
-let date = document.querySelector('#date');
-let month = document.querySelector('#month');
-const nonFormContainers = document.querySelectorAll('.non-form-containers');
-const submitCustomEvent = document.querySelector('.submit-custom-event');
-
-UI.loadAppElements(date, month, nonFormContainers);
-UI.loadUserEvents();
+document.addEventListener('DOMContentLoaded', UI.loadUserEvents);
 
 
-//add new event 
-submitCustomEvent.addEventListener('click', (e) => {
+const eventName = document.querySelector('#event-name');
+const eventDate = document.querySelector('#event-date');
+const submitEvent = document.querySelector('#submit');
+const eventsList = document.querySelector('#events-list');
+
+let id = 1;
+
+document.addEventListener('DOMContentLoaded', UI.loadUserEvents);
+
+//add custom user event 
+submitEvent.addEventListener('click', (e) => {
     e.preventDefault();
-    const eventName = document.querySelector('#event-name').value;
-    const setDate = document.querySelector('#date').value;
-    const setMonth = document.querySelector('#month').value;
-
-    if (eventName === '' || setDate === 'DATE' || setMonth === 'MONTH') {
-        UI.showAlert('Please fill in all The fields!!', 'primary');
+    if (eventName.value === '' || eventDate.value === '') {
+        UI.showAlert('Please fill in all the details!!', 'danger');
     }
     else {
-        const newEvent = new userCustomOccasions(eventName, setDate, setMonth);
-        UI.addEvent(newEvent);
-        Store.addEvent(newEvent);
-        UI.showAlert('Event Successfully Added', 'success');
-        UI.clearFields();
-        UI.closeForm();
+        const event = new userCustomOccasions(id, eventName.value, eventDate.value);
+        UI.addEvent(event);
+        Store.addEvent(event);
+        UI.showAlert('Event Added Successfully', 'success');
+        UI.clearFields(eventName, eventDate);
+        id += 1;
     }
 });
 
-
-// delete any custom event upon clicking cross in card
-document.querySelector('.events-list').addEventListener('click', (e) => {
-    UI.deleteEvent(e.target);
-    Store.deleteEvent(e.target.previousElementSibling.innerText);
-    UI.showAlert('Event Successfully Removed', 'success');
+eventsList.addEventListener('click', (e) => {
+    if (e.target.classList.contains('remove')) {
+        UI.deleteEvent(e.target.parentElement.parentElement);
+        Store.deleteEvent(e.target.parentElement.parentElement.firstElementChild.textContent);
+        UI.showAlert('Event Removed Successfully', 'success');
+    }
 });
